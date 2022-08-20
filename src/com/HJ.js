10d1300e130e094adeb75b6d601aba2d5a7f22d5
Jibran220 <jibranjibran220@gmail.com>
2022-08-19 19:16:19 -0800

    first commit

---
+++ src/com/HJ.js
@@ -0,0 +1,264 @@
+import React from "react";
+
+export const HJ = () => {
+  return (
+    <div>
+      <h3>Legal</h3>
+
+      <ul>
+        <li>For Customers</li>
+        <ul>
+          <li>Master Terms</li>
+          <li>Product Specific Terms</li>
+          <li>Data Processing Agreement</li>
+          <li>Product and Services Catalog</li>
+          <li>Acceptable Use Policy</li>
+          <li>HubSpot Security Program</li>
+          <li>Content Moderation @HubSpot</li>
+        </ul>
+        <li>For Partners</li>
+        <ul>
+          <li>App Partner Program Agreement</li>
+          <li>Solutions Partner Program Agreement</li>
+          <li>Marketing Affiliate Program Agreement</li>
+          <li>HubSpot for Startups Partner Agreement</li>
+          <li>Education Partner Agreement</li>
+        </ul>
+        <li>For Developers</li>
+        <ul>
+          <li>HubSpot Developer Terms</li>
+
+          <li>HubSpot Developer Policy</li>
+
+          <li>Acceptable Use Policy</li>
+        </ul>
+        <li>For Everyone</li>
+        <ul>
+          <li>Website Terms of Use</li>
+
+          <li>Privacy Policy</li>
+
+          <li>Cookie Policy</li>
+        </ul>
+        <li>Other Legal Docs</li>
+        <ul>
+          <li>Community Terms of Use</li>
+
+          <li>Directory Terms of Use</li>
+
+          <li>HUG Program Agreement</li>
+        </ul>
+        <li>Back to Legal Stuff home</li>
+      </ul>
+
+      <br />
+      <h3>HubSpot Privacy Policy</h3>
+      <br />
+      <p>Last Modified: February 23, 2022</p>
+      <br />
+
+      <span>
+        <p>
+          We at HubSpot and our Affiliates are committed to protecting your
+          privacy. This Privacy Policy applies to the information and data
+          collected by HubSpot when you interact with us on our websites or
+          through other channels (for example, by communicating with us or
+          attending one of our events), and when you use our products and
+          services. This Privacy Policy describes how we collect, receive, use,
+          store, share, transfer, and process your Personal Data. It also
+          describes your choices and rights with respect to your Personal Data,
+          including your rights of access and correction of your Personal Data.
+          If you do not agree with this Privacy Policy, you should not use our
+          websites or the Subscription Service.&nbsp;
+        </p>
+
+        <span>
+          Customers may collect and manage Personal Data when using the
+          Subscription Service HubSpot processes such data as a processor under
+          the direction of our customers. Our customers, as data controllers,
+          are responsible for complying with any regulations or laws that
+          require providing notice, disclosure, and/or obtaining consent prior
+          to collecting their customers’ Personal Data using the Subscription
+          Service. Please see the below section "Information We Collect and
+          Process When You Use the Subscription Service" and our{" "}
+          <span>Customer Data Processing Agreement</span>
+          for more information.&nbsp;
+        </span>
+      </span>
+
+      <span>
+        We update this Privacy Policy from time to time and we encourage you to
+        review this Privacy Policy periodically. We will post any Privacy Policy
+        changes on this page and, if the changes are material, we will provide a
+        more prominent notice by sending you an email notification, or if you
+        subscribe to our products and services, through the Notification app in
+        your HubSpot account. Terms not otherwise defined in this Privacy Policy
+        will have the meaning as set forth in the{" "}
+        <span>HubSpot Customer Terms of Service</span>
+        <p>
+          . For the purposes of this Privacy Policy “Personal Data” shall mean
+          any information relating to an identified or identifiable individual
+          where such information is protected similarly as personal data or
+          personally identifiable information under applicable Data Protection
+          Laws (as defined in the HubSpot Data Processing Agreement).
+        </p>
+        <p>
+          If you have any questions about this Privacy Policy or our treatment
+          of your Personal Data, please use
+          <span>this form</span>
+        </p>
+        <p>
+          or write to us by mail at HubSpot, Inc., 25 First Street, 2nd Floor,
+          Cambridge, MA 02141 USA, Attn: Privacy and Data Protection Officer.
+        </p>
+        <ol>
+          <li>
+            <strong>
+              <p>What Information We Collect About You</p>
+            </strong>
+          </li>
+          <li>
+            <span>
+              <strong>How We Use Your Information</strong>
+            </span>
+          </li>
+          <li>
+            <span>
+              <strong>
+                <p>How We Share Information We Collect</p>
+              </strong>
+            </span>
+          </li>
+          <li>
+            <span>
+              <strong>
+                <p>How We Transfer Information We Collect Internationally</p>
+              </strong>
+            </span>
+          </li>
+          <li>
+            <p>
+              <span>
+                <strong>How We Store and Secure Your Information</strong>
+              </span>
+            </p>
+          </li>
+          <li>
+            <span>
+              <strong>
+                <p
+                //"#Cookies-and-Similar-Technology"
+                >
+                  Cookies and Similar Technologies
+                </p>
+              </strong>
+            </span>
+          </li>
+          <li>
+            <span>
+              <strong>
+                <p
+                //"#How-To-Access-and-Control-Your-Personal-Data"
+                >
+                  How to Access &amp; Control Your Personal Data
+                </p>
+              </strong>
+            </span>
+          </li>
+          <li>
+            <p
+            //"#California-Privacy-Rights"
+            >
+              <span>
+                <strong>California Privacy Rights</strong>
+              </span>
+            </p>
+          </li>
+          <li>
+            <p
+            //"#Data-Protection-Officer"
+            >
+              <span>
+                <strong>Data Protection Officer</strong>
+              </span>
+              <span>
+                <li>
+                  <strong>Other Important Privacy Information</strong>
+                </li>
+              </span>
+            </p>
+          </li>
+        </ol>
+      </span>
+
+      <h2>
+        1<span>. What Information We Collect About You</span>
+      </h2>
+      <h3>
+        <span
+          style={{
+            fontSize: 20,
+            backgroundColor: "transparent",
+          }}
+        >
+          a. Information You Provide to HubSpot
+        </span>
+      </h3>
+      <p>
+        When you interact with us via our websites or use the Subscription
+        Service, we may collect Personal Data and other information from you, as
+        further described below.
+      </p>
+      <p>When You Visit Our Websites</p>
+      <p>
+        We collect Personal Data from you when you submit web forms or interact
+        with our websites, for example subscribing to a HubSpot blog, signing up
+        for a webinar, or requesting customer support. We may ask for your email
+        address, first and last name, job title, and other similar business
+        information. You are free to explore some of our websites without
+        providing any Personal Data about yourself.&nbsp;
+      </p>
+      <p
+        style={{
+          fontSize: 18,
+          fontWeight: "normal",
+        }}
+      >
+        INBOUND and Other Event Information&nbsp;
+      </p>
+      <p>
+        When you register for{" "}
+        <p>
+          <span>INBOUND</span>
+        </p>{" "}
+        or a HubSpot event, we collect information such as name, address, phone
+        number, and email address. We use this information to communicate with
+        you and in some cases facilitate your registration. Additionally, we may
+        share your registration data with service providers of INBOUND in order
+        to provide event information and improve your experience at INBOUND or
+        future HubSpot events.&nbsp;
+      </p>
+      <p>Account and User Information</p>
+      <p>
+        We collect Personal Data when you sign-up for a HubSpot account, create
+        or modify user information, set preferences, or provide any other
+        related information to access or utilize our Subscription Service.&nbsp;
+      </p>
+      <p>Payment Information&nbsp;</p>
+      <p>
+        We collect payment and billing information when you register for paid
+        products or services. For example, we may ask you to provide a billing
+        address, or a billing contact for your HubSpot account. If you give us
+        payment information, we use it solely as authorized by you in accordance
+        with this Privacy Policy.&nbsp;
+      </p>
+      <p>
+        You may also provide payment information, such as a credit card number
+        or bank account numbers, when purchasing products or services. We use
+        secure third-party payment service providers to manage payment
+        processing, which is collected through a secure payment
+        process.&nbsp;&nbsp;
+      </p>
+    </div>
+  );
+};
